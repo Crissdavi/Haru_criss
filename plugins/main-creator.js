0637302010e69a2f3dce8621c6e33647d3df483e
Crissdavi <158458944+Crissdavi@users.noreply.github.com>
2024-08-07 00:32:24 -0500

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;おDanịel.xyz⁩;;\nFN:おDanịel.xyz⁩\nORG:おDanịel.xyz⁩\nTITLE:\nitem1.TEL;waid=5218261275256:5218261275256\nitem1.X-ABLabel:おDanịel.xyz⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おDanịel.xyz⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;⁩ᘛCriss;;\nFN:ᘛCriss⁩\nORG:ᘛCriss\nTITLE:\nitem1.TEL;waid=51913776697:51913776697\nitem1.X-ABLabel:ᘛCriss⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:ᘛCriss⁩\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'ᘛCriss', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
